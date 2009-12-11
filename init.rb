1b1a9b3f3f584be90d7cf5dafd7d17ba9ee66c0a
Matt McMahand <mmcmahand@gmail.com>
2009-12-11 18:38:39 -0500

    version bump adding myself as a partial author :-P

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_hoptoad_server do
   name 'Hoptoad Server'
-  author 'Jan Schulz-Hofen, Marcello Barnaba'
+  author 'Jan Schulz-Hofen, Marcello Barnaba, Matt McMahand'
   description 'Redmine acts as an Hoptoad server with this plugin. Hoptoad is an HTTP(S) API, available at http://www.hoptoadapp.com/, to allow automatic reporting of application exceptions. This flavour of the plugin allows Redmine to receive plain-text notifications, useful if you use the Hoptoad facility for e.g. user feedbacks'
-  version '0.0.2'
+  version '0.0.3'
 end
